e548c20a561d08bfcf4bdd6f3b389a0a82378a91
GiulianoMauroDana <104393352+GiulianoMauroDana@users.noreply.github.com>
2025-10-04 17:55:13 -0300

    04/10/25
    laberinto armado
    completar codigo

--- Assets/playerMovment.cs
+++ Assets/playerMovment.cs
@@ -28,4 +28,22 @@ public class playerMovment : MonoBehaviour
         Vector3 forceDireccion = new Vector3(moveHorizontal, 0f, moveVertical);
         _rigibody.AddForce(forceDireccion * moveForce);
     }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, groundCheckRadius + 0.1f, groundLayer);
+    }
+
+    private void HamdelJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !IsGrounded()) 
+        {
+            _rigibody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);   
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * (groundCheckRadius + 0.1f));
+    }
 }
